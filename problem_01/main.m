bded2d0de571e3e49e20f37c13ed5e2090399f77
João Victor Oliveira Couto <commits@jictyvoo.dev>
2024-09-20 05:16:21 -0300

    :iphone: refactor: Render train on main.m

--- problem_01/main.m
+++ problem_01/main.m
@@ -7,6 +7,8 @@ setup; % This will call setup.m
 discoverFrequency = @(t) (length(t) >= 2) * (1 / abs(t(2) - t(1))) + (length(t) < 2) * 0;
 Fs = discoverFrequency(t) / 2; % amostras por segundo
 
+plt_rows = 3;
+plt_cols = 2;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Filtragem IDEAL
 %
@@ -14,16 +16,57 @@ Fs = discoverFrequency(t) / 2; % amostras por segundo
 # FILTRANDO O SINAL para reconstrução
 [Yjw_filtered, yt_reconstructed] = applyLowPassFilter(Yjw, f, Fs);
 
-# PLOTS
-plotSamplingResults('Trem de Impulsos', t, xt, impulse_train, yt, f, Yjw, Yjw_filtered, yt_reconstructed)
+% PLOTS
+figure; % Cria uma nova figura para os gráficos
+
+% 1. Sinal senoidal
+subplot(plt_rows, plt_cols, 1);
+plot(t, xt);
+title('Sinal senoidal');
+xlabel('Tempo (t)');
+ylabel('Magnitude');
+
+% 2. Train
+subplot(plt_rows, plt_cols, 2), stem(t, impulse_train);
+title('Trem de Impulsos');
+xlabel('Tempo (t)');
+ylabel('Magnitude');
+
+% 3. PAM com Train
+subplot(plt_rows, plt_cols, 3), stem(t, yt);
+title('PAM com Trem de Impulsos');
+xlabel('Tempo (t)');
+ylabel('Magnitude');
+plotSamplingResults(t, xt, impulse_train, yt, f, Yjw, Yjw_filtered, yt_reconstructed)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Filtragem NATURAL
 %
-Fs = Fs / 4;
-[pulse_train, yt, f, Yjw] = naturalModulation(xt, t, Fs);
+% Remove zeros from the array
+phi = phi(phi != 0);
+[pulse_train, yt, f, Yjw] = naturalModulation(xt, t, Fs, min(phi));
 # FILTRANDO O SINAL para reconstrução
 [Yjw_filtered, yt_reconstructed] = applyLowPassFilter(Yjw, f, Fs);
 
 # PLOTS
-plotSamplingResults('Trem de Pulsos', t, xt, pulse_train, yt, f, Yjw, Yjw_filtered, yt_reconstructed)
+figure; % Cria uma nova figura para os gráficos
+
+% 1. Sinal senoidal
+subplot(plt_rows, plt_cols, 1);
+plot(t, xt);
+title('Sinal senoidal');
+xlabel('Tempo (t)');
+ylabel('Magnitude');
+
+% 2. Train
+subplot(plt_rows, plt_cols, 2), plot(t, pulse_train);
+title('Trem de Pulsos');
+xlabel('Tempo (t)');
+ylabel('Magnitude');
+
+% 3. PAM com Train
+subplot(plt_rows, plt_cols, 3), plot(t, yt);
+title('PAM com Trem de Pulsos');
+xlabel('Tempo (t)');
+ylabel('Magnitude');
+plotSamplingResults(t, xt, pulse_train, yt, f, Yjw, Yjw_filtered, yt_reconstructed)
--- problem_01/plotSamplingResults.m
+++ problem_01/plotSamplingResults.m
@@ -1,47 +1,22 @@
-function plotSamplingResults(train_label, t, xt, train, yt, f, Yjw, Yjw_filtered, yt_reconstructed)
+function plotSamplingResults(t, xt, train, yt, f, Yjw, Yjw_filtered, yt_reconstructed)
     % PLOTS
     plt_rows = 3;
     plt_cols = 2;
-    figure; % Cria uma nova figura para os gráficos
-
-    % 1. Sinal senoidal
-    subplot(plt_rows, plt_cols, 1);
-    plot(t, xt);
-    title('Sinal senoidal');
-    xlabel('Tempo (t)');
-    ylabel('Magnitude');
-
-    % 2. Train
-    subplot(plt_rows, plt_cols, 2);
-    stem(t, train);
-    title(train_label);
-    xlabel('Tempo (t)');
-    ylabel('Magnitude');
-
-    % 3. PAM com Train
-    subplot(plt_rows, plt_cols, 3);
-    stem(t, yt);
-    title(['PAM com ' train_label]);
-    xlabel('Tempo (t)');
-    ylabel('Magnitude');
 
     % 4. FFT do sinal PAM
-    subplot(plt_rows, plt_cols, 4);
-    plot(f, abs(Yjw));
+    subplot(plt_rows, plt_cols, 4), plot(f, abs(Yjw));
     title('FFT do sinal PAM');
     xlabel('Frequência (Hz)');
     ylabel('Magnitude');
 
     % 5. Filtro de frequências
-    subplot(plt_rows, plt_cols, 5);
-    plot(f, abs(Yjw_filtered));
+    subplot(plt_rows, plt_cols, 5), plot(f, abs(Yjw_filtered));
     title('Frequências filtradas');
     xlabel('Frequência (Hz)');
     ylabel('Magnitude');
 
     % 6. Sinal reconstruído
-    subplot(plt_rows, plt_cols, 6);
-    plot(t, real(yt_reconstructed)); % Usando real() para evitar partes imaginárias
+    subplot(plt_rows, plt_cols, 6), plot(t, real(yt_reconstructed)); % Usando real() para evitar partes imaginárias
     title('Sinal reconstruído');
     xlabel('Tempo (t)');
     ylabel('Magnitude');
