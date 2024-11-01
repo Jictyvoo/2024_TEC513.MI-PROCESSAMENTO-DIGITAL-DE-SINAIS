96a4f154f028dbb22275786028baf429312d86a3
João Victor Oliveira Couto <commits@jictyvoo.dev>
2024-11-01 11:56:26 -0300

    :wrench: feat: Add a new const for sampling frequency

---
+++ problem_02/MEGA2560_Analog2Digital/sampling_frequency.hpp
@@ -0,0 +1,38 @@
+// frequency.h
+#ifndef SAMPLING_FREQUENCY_HPP
+#define SAMPLING_FREQUENCY_HPP
+
+
+// Enum to represent sampling frequency labels
+enum SamplingFrequency {
+  FREQ_125KHZ,  // 125000 Hz, OCR1A = 15 for 8x prescaler
+  FREQ_50KHZ,   // 50000 Hz, OCR1A = 39 for 8x prescaler
+  FREQ_20KHZ,   // 20000 Hz, OCR1A = 99 for 8x prescaler
+  FREQ_10KHZ,   // 10000 Hz, OCR1A = 199 for 8x prescaler
+  FREQ_5KHZ,    // 5000 Hz, OCR1A = 399 for 8x prescaler
+  FREQ_2KHZ,    // 2000 Hz, OCR1A = 999 for 8x prescaler
+  FREQ_1KHZ     // 1000 Hz, OCR1A = 124 for 128x prescaler
+};
+
+// Struct to hold OCR1A value and corresponding frequency in Hertz
+struct FrequencyInfo {
+  unsigned int ocr1aValue;   // Register value
+  unsigned int frequencyHz;  // Frequency in Hertz
+};
+
+// Array of FrequencyInfo, indexed by SamplingFrequency
+const FrequencyInfo frequencyTable[] = {
+  { 15, 125000 },  // FREQ_125KHZ
+  { 39, 50000 },   // FREQ_50KHZ
+  { 99, 20000 },   // FREQ_20KHZ
+  { 199, 10000 },  // FREQ_10KHZ
+  { 399, 5000 },   // FREQ_5KHZ
+  { 999, 2000 },   // FREQ_2KHZ
+  { 124, 1000 }    // FREQ_1KHZ, with 128x prescaler
+};
+
+// Macro to access FrequencyInfo based on SamplingFrequency enum
+#define GET_FREQUENCY_INFO(freq) (frequencyTable[freq])
+
+
+#endif  // SAMPLING_FREQUENCY_HPP
